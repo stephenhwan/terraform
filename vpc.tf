30d65ed5b9dd353a18cf15516184dfa5386910b3
stephenhwan <131231726+stephenhwan@users.noreply.github.com>
2023-06-14 16:17:54 +0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,64 +1,43 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAWCHAWBPFJESIZR5K"
-  secret_key = "9wo9aBaFSOzakWkZabc3x9FZtr9Wj6f8ODtbtXxD"
-}
+module "example_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-# Tạo VPC
-resource "aws_vpc" "example_vpc" {
-  cidr_block = "192.168.0.0/20"
+  name            = "projec-1"             # Tên của VPC
+  cidr            = "192.168.0.0/20"       # Phạm vi CIDR của VPC
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]   # Khu vực có sẵn để tạo subnet
+  public_subnets  = ["192.168.0.0/23", "192.168.2.0/23"]    # Danh sách các subnet công khai
 
   tags = {
-    Name = "projec-1"
- }
-}
-
-# Tạo Internet Gateway và liên kết với VPC
-resource "aws_internet_gateway" "example_igw" {
-  vpc_id = aws_vpc.example_vpc.id
-}
-
-# Tạo route table cho public subnet
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.example_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example_igw.id
+    Terraform   = "true"                  # Nhãn Terraform
+    Environment = "dev"                   # Nhãn môi trường
   }
 }
 
-# Tạo public subnet 1 với địa chỉ IP public thuộc availability zone a
 resource "aws_subnet" "public_subnet_a" {
-  vpc_id            = aws_vpc.example_vpc.id
-  cidr_block        = "192.168.0.0/23"
-  availability_zone = "us-west-2a"
+  vpc_id            = module.example_vpc.vpc_id               # ID của VPC
+  cidr_block        = "192.168.0.0/23"                        # Phạm vi CIDR của subnet
+  availability_zone = "ap-southeast-1a"                       # Khu vực có sẵn để tạo subnet
 
   tags = {
-    Name = "public-ip-01"
+    Name = "public-ip-01"                                    # Tên của subnet công khai
   }
 }
 
-# Gán route table cho public subnet 1
 resource "aws_route_table_association" "public_subnet_a_rt" {
-  subnet_id       = aws_subnet.public_subnet_a.id
-  route_table_id  = aws_route_table.public_rt.id
+  subnet_id       = aws_subnet.public_subnet_a.id             # ID của subnet công khai
+  route_table_id  = module.example_vpc.main_route_table_id    # ID của bảng định tuyến chính của VPC
 }
 
-# Tạo public subnet 2 với địa chỉ IP public thuộc availability zone b
 resource "aws_subnet" "public_subnet_b" {
-  vpc_id            = aws_vpc.example_vpc.id
-  cidr_block        = "192.168.2.0/23"
-  availability_zone = "us-west-2b"
+  vpc_id            = module.example_vpc.vpc_id               # ID của VPC
+  cidr_block        = "192.168.2.0/23"                        # Phạm vi CIDR của subnet
+  availability_zone = "ap-southeast-1b"                       # Khu vực có sẵn để tạo subnet
 
   tags = {
-    Name = "public-ip-02"
+    Name = "public-ip-02"                                    # Tên của subnet công khai
   }
 }
 
-# Gán route table cho public subnet 2
 resource "aws_route_table_association" "public_subnet_b_rt" {
-  subnet_id       = aws_subnet.public_subnet_b.id
-  route_table_id  = aws_route_table.public_rt.id
+  subnet_id       = aws_subnet.public_subnet_b.id             # ID của subnet công khai
+  route_table_id  = module.example_vpc.main_route_table_id    # ID của bảng định tuyến chính của VPC
 }
-.
